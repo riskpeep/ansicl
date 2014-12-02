c9c9b3266d1912bafb6b563cda02ffd5d7eabfd2
Robert Kerr <rskerr@gmail.com>
2014-12-02 17:23:28 -0500

    Started work on e3.09

--- src/e2.09.lisp
+++ src/e2.09.lisp
@@ -53,3 +53,60 @@
 (= 6 (summit (list '1 nil '2 '3)))
 (= 6 (summit2 (list '1 nil '2 '3)))
 
+;; And all the more thoroughly squished
+;; 
+;; The first function (a) attempts the sum by first removing all nils from
+;; the given list and then applying the plus function to the remainder.  The
+;; problem with this function is that it doesn't check for non-numbers in the
+;; list.  The letter A is a perfectly valid list member, but will cause plus
+;; to error because it is not of type number.  A correct implementation will
+;; use numberp to remove all non-number types from the list.
+;; 
+;; A second issue with the function (a) is that the remove line doesn't
+;; actually change the value of 1st.  remove returns a new list that has the
+;; passed values removed.  Since this returned value doesn't get assigned to
+;; anything, it is lost.  The result is that the apply function works on the
+;; originally passed list, not the list that has had nil removed.
+;;
+;; A corrected implementation is shown below.
+;; 
+(defun summit-a (1st)
+  (apply #'+
+         (remove-if (lambda (x) (not (numberp x))) 1st)))
+
+;; Test
+(= 6 (summit-a '(1 3 2)))
+
+(= 6 (summit-a '(1 3 2 a b c)))
+
+(= 6 (summit-a '(1 3 2 a b c (list d e))))
+
+;  (b) (defun summit (1st)
+;        (let ((x (car 1st)))
+;          (if (null x)
+;            (summit (cdr 1st))
+;            (+ x (summit (cdr 1st))))))
+
+;; Proceeding to (b) we find the following problems.
+;; 
+;; Function (b) suffers from the same non-number problem as (a) in that it
+;; fails to check for non-numbers in the passed list.  In addition, the if test
+;; is malformed.  As it is currently written the method never terminates.  The
+;; method needs a check to see if the cdr value is a cons cell and to return 
+;; properly if not.
+;; 
+;; A corrected implementation is shown below.
+(defun summit-b (1st)
+  (let ((x (if (numberp (car 1st))
+             (car 1st)
+             0)))
+    (if (consp (cdr 1st))
+      (+ x (summit-b (cdr 1st)))
+      x)))
+
+;; Test
+(= 6 (summit-b '(1 3 2)))
+
+(= 6 (summit-b '(1 3 2 a b c)))
+
+(= 6 (summit-b '(1 3 2 a b c (list d e))))
--- src/e3.09.lisp
+++ src/e3.09.lisp
@@ -1,74 +1,96 @@
 ;Exercise 3.09:
 ;
-;  A friend is trying to write a function that returns the sum of all the 
-;  non-nil elements in a list. He has written two versions of this function,
-;  and neither of them work. Explain what's wrong with each, and give a correct
-;  version:
-;
-;  (a) (defun summit (1st)
-;        (remove nil 1st)
-;        (apply #' + 1st))
-;
-;  (b) (defun summit (1st)
-;        (let ((x (car 1st)))
-;          (if (null x)
-;            (summit (cdr 1st))
-;            (+ x (summit (cdr 1st))))))
+;  Write a program to find the longest finite path through a network 
+;  represented as in Section 3.15. The network may contain cycles.
 
-;; We begin by reviewing the functions given in the problem.
-;; 
-;; The first function (a) attempts the sum by first removing all nils from
-;; the given list and then applying the plus function to the remainder.  The
-;; problem with this function is that it doesn't check for non-numbers in the
-;; list.  The letter A is a perfectly valid list member, but will cause plus
-;; to error because it is not of type number.  A correct implementation will
-;; use numberp to remove all non-number types from the list.
-;; 
-;; A second issue with the function (a) is that the remove line doesn't
-;; actually change the value of 1st.  remove returns a new list that has the
-;; passed values removed.  Since this returned value doesn't get assigned to
-;; anything, it is lost.  The result is that the apply function works on the
-;; originally passed list, not the list that has had nil removed.
+;; From the text, we have the shortest path algorithm as shown below
 ;;
-;; A corrected implementation is shown below.
+;; (defun shortest-path (start end net)
+;;   (bfs end (list (list start)) net))
+;;
+;; (defun bfs (end queue net)
+;;   (if (null queue)
+;;     nil
+;;     (let ((path (car queue)))
+;;       (let ((node (car path)))
+;;         (if (eql node end)
+;;           (reverse path)
+;;           (bfs end
+;;                (append (cdr queue)
+;;                        (new-paths path node net))
+;;                net))))))
+;;
+;; (defun new-paths (path node net)
+;;   (mapcar #'(lambda (n)
+;;               (cons n path))
+;;           (cdr (assoc node net))))
+;;
+;;
+
+;; We begin solution by making some observations.  First, in order to find the
+;; longest path, we'll need to make an exhaustive search of the network.  This
+;; differs from the current solution since the bfs method terminates as soon
+;; as it identifies a path.  We can modify the existing bfs method to do this
+;; search if we remove the existing termination condition, and replace it with
+;; a new condition that terminates when there are no more paths to explore.
 ;; 
-(defun summit-a (1st)
-  (apply #'+
-         (remove-if (lambda (x) (not (numberp x))) 1st)))
-
-;; Test
-(= 6 (summit-a '(1 3 2)))
-
-(= 6 (summit-a '(1 3 2 a b c)))
-
-(= 6 (summit-a '(1 3 2 a b c (list d e))))
-
-;  (b) (defun summit (1st)
-;        (let ((x (car 1st)))
-;          (if (null x)
-;            (summit (cdr 1st))
-;            (+ x (summit (cdr 1st))))))
-
-;; Proceeding to (b) we find the following problems.
+;; Second, if we do an exhastive search, it is likely that we'll identify
+;; multiple paths to the network.  When we find a solution path, then, we must
+;; check to see if it is longer than any previously identified solution.  If
+;; so, then we replace any previous path with the new one.  In our solution,
+;; we'll create a path variable that we update during the search and that is
+;; returned on completion.
+;;
+;; Thrid, the exhaustive search must account for the existence of cycles.  We
+;; can detect a cycle by checking if the next added node on a path currently
+;; exists in the path.  If it does, we've identified a cycle.
 ;; 
-;; Function (b) suffers from the same non-number problem as (a) in that it
-;; fails to check for non-numbers in the passed list.  In addition, the if test
-;; is malformed.  As it is currently written the method never terminates.  The
-;; method needs a check to see if the cdr value is a cons cell and to return 
-;; properly if not.
-;; 
-;; A corrected implementation is shown below.
-(defun summit-b (1st)
-  (let ((x (if (numberp (car 1st))
-             (car 1st)
-             0)))
-    (if (consp (cdr 1st))
-      (+ x (summit-b (cdr 1st)))
-      x)))
+;;
+;; Given these observations, we can begin our solution.
 
-;; Test
-(= 6 (summit-b '(1 3 2)))
+;; First we define a high level function to kick off the search.  Note that
+;; we refer to a modified bfs function 'bfse' to indicate that it is an
+;; exhaustive search
+(defun longest-path (start end net)
+   (bfse end (list (list start)) net))
 
-(= 6 (summit-b '(1 3 2 a b c)))
+;; Next we write our modified bfse function
+(defun bfse (end queue net)
+  (let ((longest-path nil))
+    (if (null queue)
+      ;; We're at the end, return the best-path
+      longest-path
 
-(= 6 (summit-b '(1 3 2 a b c (list d e))))
+      ;; Still more to search
+      (let ((path (car queue)))
+        (let ((node (car path)))
+          ;; Have we found a solution?
+          (if (eql node end)
+            ;; Yes, lets see if it is better than the current solution
+            (if (> (length path) (length longest-path))
+              ;; It is better, update the best-path and continue the search
+              (progn
+                (setf longest-path (reverse path))
+                (bfse end (cdr queue) net)))  ; We don't need to search on the 
+                                             ; current path anymore, so don't
+                                             ; worry about appending new paths
+                                             ; here
+            ;; No, continue the search
+            (bfse end
+                 (append (cdr queue)
+                         (new-paths path node net))  ; Add new paths from the
+                                                     ; current node to the 
+                                                     ; search space
+                 net)))))))
+
+;; Finally we define the new-paths function
+(defun new-paths (path node net)
+  (mapcar #'(lambda (n)
+              ;; Add paths, but only if the node isn't currently in the path
+              ;; (this is to prevent adding cycles)
+              (if (not (member node n))
+                (cons n path)))
+          (cdr (assoc node net))))
+
+;; Now we test
+
